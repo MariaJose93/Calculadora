820ddb82b6cf0f7fdac8b5f04b7e907e5fa37938
MariaJose <maria.jose.gomez93@email.com>
2023-08-24 11:34:46 -0400

    Menú con las 4 opciones

--- calc.js
+++ calc.js
@@ -8,11 +8,33 @@ var n1, n2, suma;
 
 seguir = prompt("Bienvenido a la Calculadora, ¿quieres seguir? s/n:     ");
 if (seguir == "s") {
-    nombre = prompt("Ingrese nombre:        ")
-    n1 = parseInt(prompt("Ingrese número 1:     "))
-    n2 = parseFloat(prompt("Ingrese número 2:       "))
-    suma = n1 + n2;
-    console.log(suma);
+    nombre = prompt("Ingrese nombre:        ");
+    opcion = parseInt(prompt("1.- Suma, 2.- Resta, 3.- Multiplicación, 4.- División:    "));
+    if (opcion== 1) {
+    console.log("Esto es suma");
+    n1 = parseInt(prompt("Ingrese número 1:     "));
+    n2 = parseFloat(prompt("Ingrese número 2:       "));
+    console.log(n1 + n2);
+    }
+    else if (opcion== 2) {
+        console.log("Esto es Resta");
+        n1 = parseInt(prompt("Ingrese número 1:     "));
+        n2 = parseFloat(prompt("Ingrese número 2:       "));
+        console.log(n1 - n2);
+        }
+        else if (opcion== 3) {
+            console.log("Esto es Multiplicación");
+            n1 = parseInt(prompt("Ingrese número 1:     "));
+            n2 = parseFloat(prompt("Ingrese número 2:       "));
+            console.log(n1 * n2);
+            }
+            else if (opcion== 4) {
+                console.log("Esto es División");
+                n1 = parseInt(prompt("Ingrese número 1:     "));
+                n2 = parseFloat(prompt("Ingrese número 2:       "));
+                console.log(n1 / n2);
+                }
+
 }
 else
     console.log("Chau");
